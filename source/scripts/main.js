f9fbc63a9991a4e231d75eb3e3851a6596b4f7f7
Siyang Liu <supersiioung@gmail.com>
2017-09-25 13:30:51 -0500

    js warnings fixed

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,15 +1,14 @@
 console.log("Hello World!");
-// element.onclick = function(){alert(‘Llama!’);}
 
 // when scrolling
 document.onscroll = function(){
     shrink_navbar();
     position_indicator();
-    console.log("scroll");
+    // console.log("scroll");
 };
 
 // nav bar change
-function shrink_navbar(event) {
+function shrink_navbar() {
     let nav_bar = document.getElementsByClassName("navbar")[0];
     let nav_top = nav_bar.getBoundingClientRect().top;
     let video_sec = document.getElementsByClassName("video-sec")[0];
@@ -41,7 +40,7 @@ function removeClass(element, className) {
 }
 
 // positon indicator
-function position_indicator(event) {
+function position_indicator() {
     let nav_bar = document.getElementsByClassName("navbar")[0];
     let nav_bottom = nav_bar.getBoundingClientRect().bottom;
 
@@ -57,16 +56,16 @@ function position_indicator(event) {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
         // at the bottom of the page
         highlight(gallary_tag, album_tag);
-        console.log("at bottom");
+        // console.log("at bottom");
     } else if (nav_bottom < albums_top) {
         deHighLight(album_tag, gallary_tag);
-        console.log("no high light");
+        // console.log("no high light");
     } else if (nav_bottom < gallary_top) {
         highlight(album_tag, gallary_tag);
-        console.log("high light album");
+        // console.log("high light album");
     } else {
         highlight(gallary_tag, album_tag);
-        console.log("high light gallary");
+        // console.log("high light gallary");
     }
 }
 
@@ -143,33 +142,33 @@ function close_model(i) {
 }
 
 let model_div_1 = document.getElementById("model1-div");
-model_div_1.addEventListener('click', function(event) {
+model_div_1.addEventListener('click', function() {
     close_model(1);
 });
 
 let model_div_2 = document.getElementById("model2-div");
-model_div_2.addEventListener('click', function(event) {
+model_div_2.addEventListener('click', function() {
     close_model(2);
 });
 
 let model_div_3 = document.getElementById("model3-div");
-model_div_3.addEventListener('click', function(event) {
+model_div_3.addEventListener('click', function() {
     close_model(3);
 });
 
 
 let album_img_1 = document.getElementById("album-img-1");
-album_img_1.addEventListener('click', function(event) {
+album_img_1.addEventListener('click', function() {
     open_model(1);
 });
 
 let album_img_2 = document.getElementById("album-img-2");
-album_img_2.addEventListener('click', function(event) {
+album_img_2.addEventListener('click', function() {
     open_model(2);
 });
 
 let album_img_3 = document.getElementById("album-img-3");
-album_img_3.addEventListener('click', function(event) {
+album_img_3.addEventListener('click', function() {
     open_model(3);
 });
 
@@ -181,7 +180,7 @@ let slides = document.getElementsByClassName("slide");
 
 let next = document.getElementById('next_btn');
 let prev = document.getElementById('prev_btn');
-next.addEventListener('click', function(event) {
+next.addEventListener('click', function() {
     if (currSlide === slides.length - 1) {
         return;
     }
@@ -189,7 +188,7 @@ next.addEventListener('click', function(event) {
     slider.children[0].style.marginLeft = (-100*currSlide)+"%";
 
 });
-prev.addEventListener('click', function(event) {
+prev.addEventListener('click', function() {
     if (currSlide === 0) {
         return;
     }
